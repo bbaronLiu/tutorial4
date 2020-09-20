7fdc59d96ba52644200bd09c4c9f9f588a3c845b
Baron Liu <bbaronliu@gmail.com>
2020-09-19 22:35:32 -0700

    commit

--- js/ex1.js
+++ js/ex1.js
@@ -2,3 +2,24 @@
  Example 1 JavaScript code
 */
 console.log("Liu's Output from Tutorial 4 Example 1");
+
+var name = prompt("Please enter your name", "Enter Name");
+
+var units = prompt("Please enter units completed", "Enter Units");
+while (units <0 ) {
+    var units = prompt("Units must be >= 0, please enter again")
+}
+
+function gradeStanding(units) {
+    if (units <= 30) {
+        return "Freshman"
+    } else if (units <= 60) {
+        return "Sophomore"
+    } else if (units <= 90) {
+        return "Junior"
+    } else {
+        return "Senior"
+    }
+}
+
+print ()
