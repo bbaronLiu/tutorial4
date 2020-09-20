bb3dcad06d11ed513e60fa709bc22d68697f9a31
Baron Liu <bbaronliu@gmail.com>
2020-09-20 00:04:25 -0700

    ff

--- js/ex3.js
+++ js/ex3.js
@@ -6,25 +6,29 @@ x = 0;
 y = 0;
 var i = prompt("Enter integer");
 var ia = prompt("Enter second integer");
-
-while (x == 0) {
-    if (Number.isInteger(i) == "true") {
-        break;
-    } else {
-        var i = prompt("Enter an integer please");
+function check(){
+    while (x == 0) {
+        if (Number.isInteger(i) == "true") {
+            return i;
+        } else {
+            var i = prompt("Enter an integer please");
+        }
     }
 }
 
-while (y == 0) {
-    if (Number.isInteger(ia)) {
-        break;
-    } else {
-        var ia = prompt("Enter a second integer please");
+function checkia() {
+    while (y == 0) {
+        if (Number.isInteger(ia)) {
+            return ia;
+        } else {
+            var ia = prompt("Enter a second integer please");
+        }
     }
 }
 
-console.log(i + ia);
-console.log(i - ia);
-console.log(i * ia);
-console.log(i / ia);
-console.log(i % ia);
+
+console.log(check() + checkia());
+console.log(check() - checkia());
+console.log(check() * checkia());
+console.log(check() / checkai());
+console.log(check() % checkia());
