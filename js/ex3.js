4ae09357a6750c565774d778efabb5711398b000
Baron Liu <bbaronliu@gmail.com>
2020-09-19 23:47:04 -0700

    3

--- js/ex3.js
+++ js/ex3.js
@@ -5,18 +5,19 @@ console.log("Liu's Output from Tutorial 4 Example 1");
 x = 0
 y = 0
 var i = prompt("Enter integer");
+var ia = prompt("Enter second integer");
+
 while (x == 0) {
     if (Number.isInteger(i) == true) {
-        break;
+        x = 1;
     } else {
         var i = prompt("Enter an integer please");
     }
 }
 
 while (y == 0) {
-    var ia = prompt("Enter second integer")
     if (Number.isInteger(ia) == true) {
-        break;
+        x = 1;
     } else {
         var ia = prompt("Enter a second integer please");
     }
