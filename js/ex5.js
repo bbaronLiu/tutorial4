4ef7d525394fe29274c547a80147c856f976b4b8
Baron Liu <bbaronliu@gmail.com>
2020-09-20 00:40:05 -0700

    fwfwefewf

--- js/ex5.js
+++ js/ex5.js
@@ -2,3 +2,10 @@
  Example 1 JavaScript code
 */
 console.log("Liu's Output from Tutorial 4 Example 1");
+
+var number = prompt("Enter a number")
+x = 0;
+while (x < 11) {
+    console.log(number + " * " + x " = " (x * number));
+    x = x + 1;
+}
