893adfecef01a6483e387aa9cd6160921cb52dda
Baron Liu <bbaronliu@gmail.com>
2020-09-20 00:18:51 -0700

    fsefr

--- js/ex3.js
+++ js/ex3.js
@@ -2,21 +2,21 @@
  Example 1 JavaScript code
 */
 console.log("Liu's Output from Tutorial 4 Example 1");
-x = true;
 
-while (x == true) {
-    var i = prompt("enter an integer")
-    if (Number.isInteger(i)) {
-        break;
+
+function getInt() {
+    let input;
+    while (input = prompt("Input number: ")) {
+        if (isNaN(input)) {
+            alert("Invalid input.");
+        } else {
+            return parseInt(input);
+        }
     }
 }
 
-while (x == true) {
-    var ia = prompt("enter an integer")
-    if (Number.isInteger(i)) {
-        break;
-    }
-}
+var i = getInt()
+var ia = getInt()
 
 console.log(i + ia);
 console.log(i - ia);
