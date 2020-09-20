97adb1ca843a1e566e991f1b4847cbe3d578c03d
Baron Liu <bbaronliu@gmail.com>
2020-09-19 23:38:31 -0700

    commit ex3

--- js/ex3.js
+++ js/ex3.js
@@ -2,3 +2,23 @@
  Example 1 JavaScript code
 */
 console.log("Liu's Output from Tutorial 4 Example 1");
+
+var i = prompt("Enter integer");
+if (Number.isInteger(i) == false) {
+    break;
+} else {
+    var i = prompt("Enter an integer please");
+}
+
+var ia = prompt("Enter second integer")
+if (Number.isInteger(ia) == false) {
+    break;
+} else {
+    var ia = prompt("Enter a second integer please");
+}
+
+console.log(i + ia);
+console.log(i - ia);
+console.log(i * ia);
+console.log(i / ia);
+console.log(i % ia);
