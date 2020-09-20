a2d4ad71e5d67673c5ae38becd0878f9d0fae1a3
Baron Liu <bbaronliu@gmail.com>
2020-09-19 23:52:39 -0700

    fixed

--- js/ex3.js
+++ js/ex3.js
@@ -2,13 +2,13 @@
  Example 1 JavaScript code
 */
 console.log("Liu's Output from Tutorial 4 Example 1");
-x = 0
-y = 0
+x = 0;
+y = 0;
 var i = prompt("Enter integer");
 var ia = prompt("Enter second integer");
 
 while (x == 0) {
-    if (Number.isInteger(i) == true) {
+    if (Number.isInteger(i)) {
         x = 1;
     } else {
         var i = prompt("Enter an integer please");
@@ -16,7 +16,7 @@ while (x == 0) {
 }
 
 while (y == 0) {
-    if (Number.isInteger(ia) == true) {
+    if (Number.isInteger(ia)) {
         x = 1;
     } else {
         var ia = prompt("Enter a second integer please");
