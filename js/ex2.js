3b4ed239b2800d2006d70ce627b382a6cf412378
Baron Liu <bbaronliu@gmail.com>
2020-09-19 23:13:25 -0700

    c

--- js/ex2.js
+++ js/ex2.js
@@ -2,3 +2,39 @@
  Example 2 JavaScript code
 */
 console.log("Liu's Output from Tutorial 4 Example 2");
+
+var day = prompt("enter 3 letter abbreviation day of the week e.g. sun/mon");
+
+function followingDay(function repeat()) {
+    while (['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'].includes(day)) {
+        switch (day) {
+            case "sun":
+                day = "mon";
+                break;
+            case "mon":
+                day = "tue";
+                break;
+            case "tue":
+                day = "wed";
+                break;
+            case "wed":
+                day = "thu";
+                break;
+            case "thu":
+                day = "fri";
+                break;
+            case "fri":
+                day = "sat";
+                break;
+            case "sat":
+                day = "sun";
+                break;
+        } else {
+            var day = prompt("please enter a correct date");
+        }
+    
+    }
+}
+
+console.log ("You entered : " + day);
+console.log ("The following day is: " + followingDay(day));
