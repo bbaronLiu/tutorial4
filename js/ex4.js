f1e88d9ce1c1c0e75e8b86ecdaec842e59ed25f4
Baron Liu <bbaronliu@gmail.com>
2020-09-20 00:33:32 -0700

    ergerthgthh

--- js/ex4.js
+++ js/ex4.js
@@ -6,14 +6,14 @@ console.log("Liu's Output from Tutorial 4 Example 1");
 function passwordCheck() {
     var x = 0;
     while (true) {
+        var input = prompt("Enter password: ")
         if (input == "secret") {
             return ("You entered the correct password after " + (x+1) + "attempt(s)")
-        } else {
-            x = x + 1;
         }
-        var input = prompt("Enter password: ")
         if (x > 2) {
             return ("Your account is locked! You failed to enter the correct password " + x + "times")
+        } else {
+            x = x + 1;
         }
     }
 }
